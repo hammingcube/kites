a45a44eae70d1532ef96df9d6f90c14062f05fab
maddyonline <madhavjha@gmail.com>
2015-05-10 04:58:14 -0700

    More

--- draft/db-exp/db/db.go
+++ draft/db-exp/db/db.go
@@ -1,24 +1,10 @@
 package db
 
-import "gopkg.in/mgo.v2"
-import "time"
-
-const DIAL_TIMEOUT = time.Duration(2)*time.Second
-
-type Config struct {
-	Host	string
+type Saver interface {
+	Connect(location string) error
+	Read(location string, key []byte) ([]byte, error)
+	Save(location string, data []byte) (interface{}, error)
 }
 
-type Database struct {
-	Cfg	Config
-	Session *mgo.Session
-}
 
-func NewConnection(host string) (*Database, error) {
-	db := &Database{Cfg: Config{Host: host}}
-	session, err := mgo.DialWithTimeout(host, DIAL_TIMEOUT)
-	if err == nil {
-		db.Session = session
-	}
-	return db, err
-}
+
--- draft/db-exp/db/db_test.go
+++ draft/db-exp/db/db_test.go
@@ -2,10 +2,7 @@ package db
 
 import (
 	"testing"
-	"fmt"
 )
 
 func TestMongDB(t *testing.T) {
-	db, err := NewConnection("localhost")
-	fmt.Printf("%v %v", db, err)
 }
