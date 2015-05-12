55b129a00bb87c05f24be93c275446f8d116810f
maddyonline <madhavjha@gmail.com>
2015-05-11 22:04:14 -0700

    a workable mgo db

--- draft/db-exp/main.go
+++ draft/db-exp/main.go
@@ -1,9 +1,64 @@
 package main
 
 import (
-	"fmt"
+	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"log"
 )
 
+const DB_HOST = "127.0.0.1"
+
+var dbSession *mgo.Session
+
+type Users struct {
+	db string
+	coll string 
+}
+
+type User struct {
+	Name	string
+	Email	string
+}
+
+func (users *Users) Save(u *User) error {
+	var err error
+	session := dbSession.Copy()
+    defer session.Close()
+    if c := session.DB(users.db).C(users.coll); c != nil {
+    	err = c.Insert(u)
+    }
+    return err
+}
+
+func (users *Users) Read(email string) (*User, error) {
+	var err error
+	session := dbSession.Copy()
+    defer session.Close()
+    if c := session.DB(users.db).C(users.coll); c != nil {
+    	result := &User{}
+    	err = c.Find(bson.M{"email": email}).One(&result)
+    	return result, err
+    }
+    return nil, err
+}
+
 func main() {
-	fmt.Println("Hello")
+	var err error
+	dbSession, err = mgo.Dial(DB_HOST)
+	if err != nil {
+		log.Fatal("Error connecting to database: ", err)
+	}
+	dbSession.SetMode(mgo.Monotonic, true)
+	defer dbSession.Close()
+
+	users := &Users{"myfavdb", "users"}
+	users.Save(&User{"John Doe", "jdoe@example.com"})
+	email := "jdoe@example.com"
+	user, err := users.Read(email)
+	if err != nil {
+		log.Printf("Error when finding user %s: %v", email, err)
+	} else {
+		log.Printf("%s", user)
+	}
+
 }
