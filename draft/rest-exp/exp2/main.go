1a574d7795793077c06e744b129499e7fcbd3969
maddyonline <madhavjha@gmail.com>
2015-05-13 01:48:48 -0700

    More'

--- draft/rest-exp/exp2/main.go
+++ draft/rest-exp/exp2/main.go
@@ -2,10 +2,16 @@ package main
 
 import (
 	"github.com/ant0ine/go-json-rest/rest"
+	"net/http"
 	"log"
 )
 
 func main() {
 	api := rest.NewApi()
-	log.Printf("%v", api)
+	api.Use(rest.DefaultDevStack...)
+	app := rest.AppSimple(func(w rest.ResponseWriter, r *rest.Request){
+			w.WriteJson(map[string]string{"Body": "Hello World!"})
+		})
+	api.SetApp(app)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
