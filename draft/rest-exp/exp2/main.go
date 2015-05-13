968fb31158b5800404d12d6d928ec0f0f020f08f
maddyonline <madhavjha@gmail.com>
2015-05-13 02:43:50 -0700

    basic get gist api

--- draft/rest-exp/exp2/main.go
+++ draft/rest-exp/exp2/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"github.com/ant0ine/go-json-rest/rest"
+	"net/http"
+	"log"
+)
 
 type Gist struct {
 	Id    string
@@ -8,16 +12,50 @@ type Gist struct {
 }
 
 type File struct {
+	Name	string
 	Content  string
 	Language string
 }
 
 var (
-	gist1 = Gist{"abc123", []File{{"This is amazing", "md"}, {"def func: pass", "py"}}}
-	gist2 = Gist{"pqr321", []File{{"Another file", "md"}, {"print('hello')", "py"}}}
-	fixture = []Gist{gist1, gist2}
+	gist1 = Gist{"abc123", []File{{"abc.md", "This is amazing", "Markdown"}, {"foo.py", "def func: pass", "Python"}}}
+	gist2 = Gist{"pqr321", []File{{"pqr.md", "Another file", "Markdown"}, {"main.py", "print('hello')", "Python"}}}
+	gists = []Gist{gist1, gist2}
 )
 
+type GistServer struct {
+}
+
+func (gs *GistServer) List(w rest.ResponseWriter, r *rest.Request) {
+	w.WriteJson(gists)
+}
+
+func (gs *GistServer) Get(w rest.ResponseWriter, r *rest.Request) {
+	id := r.PathParam("id")
+	var g *Gist
+	for _, gist := range gists {
+		if gist.Id == id {
+			g = &gist
+			break
+		} 
+	}
+	if g == nil {
+		rest.NotFound(w, r)
+        return
+	}
+	w.WriteJson(g)
+}
+
 func main() {
-	fmt.Println(fixture)
+	gs := &GistServer{}
+	api := rest.NewApi()
+	api.Use(rest.DefaultDevStack...)
+	router, err := rest.MakeRouter(
+		rest.Get("/gists", gs.List),
+		rest.Get("/gists/:id", gs.Get),)
+	if err != nil {
+		log.Fatal(err)
+	}
+	api.SetApp(router)
+	log.Fatal(http.ListenAndServe(":8080", api.MakeHandler()))
 }
